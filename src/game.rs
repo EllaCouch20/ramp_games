4fd3bfff12a2b007b8882167e6173f9737ecc093
Ella Mae <ella@orange.me>
2025-06-23 11:12:04 -0400

    wip<

--- src/game.rs
+++ src/game.rs
@@ -5,60 +5,43 @@ use pelican_ui::{Context, Component};
 
 use pelican_ui_std::{Stack, Content, Header, Bumper, Page, Button, Offset, TextStyle, Text, AppPage, Size, Padding};
 
-use pelican_game_engine::{CollisionEvent, AspectRatio, Sprite, GameGrid, GameboardBackground};
+use pelican_game_engine::{CollisionEvent, AspectRatio, Sprite, GameGrid, GameboardBackground, Gameboard};
 
 use std::collections::HashMap;
 
 // use crate::structs::Player;
+
+const STEP: f32 = 5.0;
 
 #[derive(Debug)]
 pub struct Galaga;
 
 impl Galaga {
     pub fn new(ctx: &mut Context) -> Gameboard {
-        let mut gameboard = Gameboard::new(ctx, AspectRatio::OneOne);
+        let mut gameboard = Gameboard::new(ctx, AspectRatio::OneOne, Box::new(Self::on_event));
         let player = Sprite::new(ctx, "player", "ship.png", (30.0, 30.0), (Offset::Center, Offset::End));
         gameboard.insert_sprite(ctx, player);
 
         gameboard
     }
-}
 
-#[derive(Debug, Component)]
-pub struct Gameboard(GameGrid, GameboardBackground, HashMap<String, Sprite>);
-
-impl Gameboard {
-    pub fn new(ctx: &mut Context, aspect_ratio: AspectRatio) -> Self {
-        let colors = ctx.theme.colors;
-        let background = GameboardBackground::new(ctx, 1.0, 8.0, colors.background.secondary, aspect_ratio);
-        Gameboard(GameGrid::new(HashMap::from([("background".to_string(), (Offset::Start, Offset::Start))]), aspect_ratio), background, HashMap::new())
-    }
-
-    pub fn insert_sprite(&mut self, ctx: &mut Context, sprite: Sprite) {
-        // let (maxw, maxh) = self.0.size(ctx);
-        // location.0 = Offset::Static(location.0.get(sprite.dimensions().0, maxw));
-        // location.1 = Offset::Static(location.1.get(sprite.dimensions().1, maxh));
-        self.0.0.insert(sprite.id().to_string(), *sprite.offset());
-        self.2.insert(sprite.id().to_string(), sprite);
-    }
-}
-
-impl OnEvent for Gameboard {
-    fn on_event(&mut self, ctx: &mut Context, event: &mut dyn Event) -> bool {
+    fn on_event(board: &mut Gameboard, ctx: &mut Context, event: &mut dyn Event) -> bool {
         if let Some(TickEvent) = event.downcast_ref::<TickEvent>() {
-            let (maxw, maxh) = self.0.size(ctx);
-            self.2.iter_mut().for_each(|(_, s)| {
-                if let Some(location) = self.0.0.get_mut(s.id()) {
-                    location.0 = Offset::Static(s.offset().0.get(s.dimensions().0, maxw).abs());
-                    location.1 = Offset::Static(s.offset().1.get(s.dimensions().1, maxh).abs());
+            let (maxw, maxh) = board.0.size(ctx);
+            board.2.iter_mut().for_each(|(_, s)| {
+                if let Some(location) = board.0.0.get_mut(s.id()) {
+                    let sd = s.dimensions().clone();
+                    location.0 = Offset::Static(s.offset().0.get(sd.0, maxw).abs() + s.position().0);
+                    location.1 = Offset::Static(s.offset().1.get(sd.1, maxh).abs() + s.position().1);
                 }
+                *s.dimensions() = (maxw / 20.0, maxw / 20.0);
             });
-            // let (maxw, maxh) = self.dimensions();
-            // let x = self.0.0.get(max_size.0, maxw);
-            // let y = self.0.1.get(max_size.1, maxh);
+            // let (maxw, maxh) = board.dimensions();
+            // let x = board.0.0.get(max_size.0, maxw);
+            // let y = board.0.1.get(max_size.1, maxh);
             // (x, y)
             // let GameboardSize(maxw, maxh) = ctx.state().get::<GameboardSize>();
-            // let children = &self.2;
+            // let children = &board.2;
 
             // children.iter().enumerate().for_each(|(i, a)| {
             //     let (ax, ay) = a.position((maxw, maxh));
@@ -77,19 +60,21 @@ impl OnEvent for Gameboard {
         } else if let Some(KeyboardEvent{state: KeyboardState::Pressed, key}) = event.downcast_ref::<KeyboardEvent>() { 
             match key {
                 Key::Named(NamedKey::ArrowLeft) => {
-                    let (maxw, maxh) = self.0.size(ctx);
-                    let movement_speed = 5.0;
-                    if let Some(player) = self.2.get_mut("player") {
-                        let offsets = self.0.0.get_mut("player").unwrap();
-                        if let Offset::Static(a) = &mut offsets.0 {*a -= movement_speed;}
+                    let (maxw, maxh) = board.0.size(ctx);
+                    if let Some(player) = board.2.get_mut("player") {
+                        let pw = player.dimensions().0;
+                        if player.offset().0.get(pw, maxw).abs() + player.position().0 > 5.0 {
+                            player.position().0 -= STEP;
+                        }
                     }
                 },
                 Key::Named(NamedKey::ArrowRight) => {
-                    let (maxw, maxh) = self.0.size(ctx);
-                    let movement_speed = 5.0;
-                    if let Some(player) = self.2.get_mut("player") {
-                        let offsets = self.0.0.get_mut("player").unwrap();
-                        if let Offset::Static(a) = &mut offsets.0 {*a += movement_speed;}
+                    let (maxw, maxh) = board.0.size(ctx);
+                    if let Some(player) = board.2.get_mut("player") {
+                        let pw = player.dimensions().0;
+                        if player.offset().0.get(pw, maxw).abs() + player.position().0 < maxw - pw - 5.0 {
+                            player.position().0 += STEP;
+                        }
                     }
                 },
                 _ => {}
